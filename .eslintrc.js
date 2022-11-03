1fd6d7aad7b91854bcb80bc409c9d9a748ee9fe1
Rafał Majewski <goodheropl@gmail.com>
2022-11-03 01:44:17 +0100

    Add ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+	root: true,
+	overrides: [
+		{
+			files: ["*.ts"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				project: "./tsconfig.json",
+				tsconfigRootDir: __dirname,
+				sourceType: "module",
+				ecmaVersion: 13,
+			},
+			extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+			plugins: ["@typescript-eslint"],
+		},
+		{
+			files: ["*.test.ts"],
+			parser: "@typescript-eslint/parser",
+			parserOptions: {
+				project: "./tsconfig.json",
+				tsconfigRootDir: __dirname,
+				sourceType: "module",
+				ecmaVersion: 13,
+			},
+			extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+			plugins: ["@typescript-eslint"],
+		},
+		{
+			files: ["*.js"],
+			parser: "espree",
+			parserOptions: {
+				ecmaVersion: 13,
+				sourceType: "module",
+			},
+			extends: ["eslint:recommended", "prettier"],
+		},
+		{
+			files: ["*.test.js"],
+			parser: "espree",
+			parserOptions: {
+				ecmaVersion: 13,
+				sourceType: "module",
+			},
+			extends: ["eslint:recommended", "prettier"],
+			plugins: [],
+		},
+	],
+	settings: {},
+	env: {
+		es2022: true,
+		node: true,
+	},
+};
