a6e7cd293670c92724286735bd1c7d7ab088381e
Rafał Majewski <goodheropl@gmail.com>
2022-12-24 20:34:06 +0100

    Move reading .env to appConfig

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import {Hello} from "./hello/index.js";
-import dotenv from "dotenv";
-dotenv.config();
 import {appConfig} from "./app-config/index.js";
 
 const hello = new Hello(appConfig.OPTIONAL_HELLO_MESSAGE);
