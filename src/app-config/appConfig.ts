03e1cc4dd42733fef7a8abe2a1f75cc6f49aeb0a
Rafał Majewski <goodheropl@gmail.com>
2022-12-24 20:34:06 +0100

    Move reading .env to appConfig

--- src/app-config/appConfig.ts
+++ src/app-config/appConfig.ts
@@ -1,4 +1,6 @@
 import {cleanEnv} from "envalid";
+import dotenv from "dotenv";
+dotenv.config();
 import appConfigSchema from "./appConfigSchema.js";
 
 const appConfig = cleanEnv(process.env, appConfigSchema);
