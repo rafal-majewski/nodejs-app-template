b1488cf205fd91a01fd414ee3f06286b8059dde6
Rafał Majewski <goodheropl@gmail.com>
2023-08-17 16:26:42 +0200

    Reconfigure ESLint

--- src/hello/Hello.ts
+++ src/hello/Hello.ts
@@ -1,6 +1,6 @@
 export class Hello {
 	private readonly message: string;
-	constructor(message: string) {
+	public constructor(message: string) {
 		this.message = message;
 	}
 	public getMessage(): string {
