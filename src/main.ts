2eb0daf5051278cfa7cce52d9feb662c38219de0
Rafał Majewski <goodheropl@gmail.com>
2023-12-05 21:18:59 +0100

    Use camel case for directory naming

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import {Hello} from "./hello/Hello.js";
-import {appConfig} from "./instances/app-config/appConfig.js";
+import {appConfig} from "./instances/appConfig/appConfig.js";
 
 const hello = new Hello(appConfig.helloMessage);
 console.log(hello.getMessage());
