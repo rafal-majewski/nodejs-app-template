dc7357dfbe1736c7a278225bc56e586d12a7cd74
Rafał Majewski <goodheropl@gmail.com>
2024-04-16 17:03:00 +0200

    Standardize newlines

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import {Hello} from "./hello/Hello.js";
 import {appConfig} from "./instances/appConfig/appConfig.js";
-
 const hello = new Hello(appConfig.helloMessage);
 console.log(hello.getMessage());
