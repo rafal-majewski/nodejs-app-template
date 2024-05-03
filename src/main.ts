82803d1482a30b2962a6939beb9717010c872b05
Rafał Majewski <goodheropl@gmail.com>
2024-05-03 21:55:16 +0200

    chore!: Fajna zmiana

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,4 @@ import {Hello} from "./hello/Hello.js";
 import {appConfig} from "./instances/appConfig/appConfig.js";
 const hello = new Hello(appConfig.helloMessage);
 console.log(hello.getMessage());
+//
