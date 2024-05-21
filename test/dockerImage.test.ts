962758665a2cd43b8311149433d1f30ae408debe
Rafał Majewski <goodheropl@gmail.com>
2024-05-21 10:15:48 +0200

    chore: Parameterize Alpine version used in the Dockerfile

--- test/dockerImage.test.ts
+++ test/dockerImage.test.ts
@@ -1,11 +1,13 @@
 import * as Test from "node:test";
 import * as Testcontainers from "testcontainers";
+const dockerImageAlpineVersion = "3.19";
 const dockerImageNodeVersion = "22.2.0";
 
 await Test.describe("Docker image", async (): Promise<void> => {
 	await Test.it("builds", async (): Promise<void> => {
 		await new Testcontainers.GenericContainerBuilder(".", "Dockerfile")
 			.withBuildArgs({
+				ALPINE_VERSION: dockerImageAlpineVersion,
 				NODE_VERSION: dockerImageNodeVersion,
 			})
 			.build();
@@ -16,6 +18,7 @@ await Test.describe("Docker container", async (): Promise<void> => {
 	await Test.it("starts", async (): Promise<void> => {
 		const container = await new Testcontainers.GenericContainerBuilder(".", "Dockerfile")
 			.withBuildArgs({
+				ALPINE_VERSION: dockerImageAlpineVersion,
 				NODE_VERSION: dockerImageNodeVersion,
 			})
 			.build();
